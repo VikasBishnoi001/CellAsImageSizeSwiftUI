d63731752f29269bc10bf9eaced9fe35ed62d5b5
Vikas Bishnoi <vikasbishnoi@leewayhertz.com>
2023-11-15 11:53:47 +0530

    Initial Commit

---
+++ CellAsImageSizeSwiftUI/CellAsImageSizeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CellAsImageSizeSwiftUIApp.swift
+//  CellAsImageSizeSwiftUI
+//
+//  Created by Leewayhertz on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CellAsImageSizeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
